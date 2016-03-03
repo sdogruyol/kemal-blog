eebb6702ac4c463f5f1dcb0a104e9071eb51b1c0
Serdar Dogruyol <dogruyolserdar@gmail.com>
2016-03-03 09:53:55 +0200

    Add show

--- src/kemal-blog.cr
+++ src/kemal-blog.cr
@@ -11,31 +11,33 @@ CONN_OPTS = {
 
 mysql_connect CONN_OPTS
 
-module Kemal::Blog
+# Index
+get '/' do |env|
+  env.redirect "/articles"
+end
 
-  get '/' do |env|
-    env.redirect "/articles"
-  end
+# New article form
+get "/articles/new" do |env|
+  render "src/views/articles/new.ecr"
+end
 
-  # New article form
-  get "/articles/new" do |env|
-    render "src/views/articles/new.ecr"
-  end
+# Lists all articles
+get "/articles" do |env|
+  articles = conn.query "SELECT * FROM articles"
+  render "src/views/articles/index.ecr"
+end
 
-  # Lists all articles
-  get "/articles" do |env|
-    articles = conn.query "SELECT * FROM articles"
-    render "src/views/articles/index.ecr"
-  end
+# Show Article with given id
+get "/articles/:id" do |env|
+  id = env.params["id"]
+  article = conn.query "SELECT * FROM articles WHERE articles.id=#{id} LIMIT 1"
+  article = article.not_nil!.first
+  render "src/views/articles/show.ecr"
+end
 
-  # Show Article with given id
-  get "/articles/:id" do
-  end
-
-  post "/articles" do |env|
-    title = env.params["title"] as String
-    body = env.params["body"] as String
-    conn.query "INSERT INTO articles(title, body) VALUES ('#{title}', '#{body}')"
-    env.redirect "/articles"
-  end
+post "/articles" do |env|
+  title = env.params["title"] as String
+  body = env.params["body"] as String
+  conn.query "INSERT INTO articles(title, body) VALUES ('#{title}', '#{body}')"
+  env.redirect "/articles"
 end
